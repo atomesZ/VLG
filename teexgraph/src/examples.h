974aba5313b902a1d3628a3dbf3edf33a6d3107c
Etienne Sharpin <etienne.sharpin@epita.fr>
2021-06-02 16:39:52 +0200

    ADD: test with clemence magnien's data

---
+++ teexgraph/src/examples.h
@@ -0,0 +1,10 @@
+/*
+ * teexGraph --- by Frank Takes --- https://github.com/franktakes/teexgraph
+ * 
+ * Examples of how to use the code
+ */
+
+#include "BDGraph.h"
+
+void showDistanceDistribution(Graph & G); 
+void stats(Graph & G);
